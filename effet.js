b726eebcff27d169f08924dda136ea399c3580f6
johangeff <johangeff@gmail.com>
2025-03-10 10:33:00 -0400

    tel

--- effet.js
+++ effet.js
@@ -1,13 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
     const navbarLinks = document.querySelectorAll("#navbar ul li a");
     const navbarHeight = document.querySelector("header").offsetHeight;
-    const productImages = document.querySelectorAll("#produits .image-container");
 
     window.addEventListener("scroll", () => {
         let currentSection = "";
         document.querySelectorAll("section").forEach((section) => {
             const sectionTop = section.offsetTop - navbarHeight;
-            if (pageYOffset >= sectionTop - 60) {
+            if (window.pageYOffset >= sectionTop - 60) {
                 currentSection = section.getAttribute("id");
             }
         });
@@ -18,23 +17,22 @@ document.addEventListener("DOMContentLoaded", () => {
                 link.classList.add("active");
             }
         });
-
-        
-
-        if (window.innerWidth <= 768) {  // Appliquer l'effet uniquement sur mobile
-            const productImages = document.querySelectorAll("#produits .image-container");
-    
-            window.addEventListener("scroll", () => {
-                productImages.forEach((image) => {
-                    const rect = image.getBoundingClientRect();
-                    // Si l'image est dans la fenêtre de visualisation, on applique la classe visible
-                    if (rect.top < window.innerHeight && rect.bottom > 0) {
-                        image.classList.add("visible");
-                    } else {
-                        image.classList.remove("visible");
-                    }
-                });
-            });
-        }
     });
+
+    if (window.innerWidth <= 768) {  // Appliquer l'effet uniquement sur mobile
+        const productImages = document.querySelectorAll("#produits .image-container");
+
+        window.addEventListener("scroll", () => {
+            productImages.forEach((image) => {
+                const rect = image.getBoundingClientRect();
+                // Si l'image est dans la fenêtre de visualisation, on applique la classe visible
+                if (rect.top < window.innerHeight && rect.bottom > 0) {
+                    image.classList.add("visible");
+                } else {
+                    image.classList.remove("visible");
+                }
+            });
+        });
+    }
 });
+
